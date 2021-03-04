042709b5c1c2b022f15749edea0cf941187cf4e5
alainvaz <alainvaz@gmail.com>
2021-03-04 16:18:42 -0300

    Adding databe access

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@
 module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
