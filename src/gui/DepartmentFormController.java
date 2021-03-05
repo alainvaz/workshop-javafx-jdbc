678cf8cd3013b2f15f4ed0f7a22e00f12908007e
alainvaz <alainvaz@gmail.com>
2021-03-05 09:36:58 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbExeption;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -27,13 +35,41 @@ public class DepartmentFormController implements Initializable{
 	private Button btCancel;
 	
 	@FXML
-	public void onBtSaveAction() {
+	public void onBtSaveAction(ActionEvent event) {
 		System.out.println("onBtSaveAction");
+		
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {			
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+			
+		}catch(DbExeption e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 	
+	private Department getFormData() {
+		
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
 	@FXML
-	public void onBtCancelAction() {
+	public void onBtCancelAction(ActionEvent event) {
 		System.out.println("onBtCancelAction");
+		Utils.currentStage(event).close();
 	}
 
 	@Override
@@ -49,6 +85,10 @@ public class DepartmentFormController implements Initializable{
 	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	public void updateFormData() {
